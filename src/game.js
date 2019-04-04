715f3ae601f01562c13b7814b0127b4eacbd9dbe
Ariel Fogel <fogeltine@gmail.com>
2019-04-04 15:29:09 -0700

    turn game into a stateless functional component

--- src/game.js
+++ src/game.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import {Board} from './board';
+import Board from './board';
 
-export default class Game extends React.Component {
-  render() {
-    return (
-      <div className='game'>
-        <div className='game-board'>
-          <Board />
-        </div>
+export default function Game(props){
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board />
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 
