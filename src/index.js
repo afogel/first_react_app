e7d3ffce317c30a1bee18e0476023ed4efd34b9e
Ariel Fogel <fogeltine@gmail.com>
2019-04-04 15:21:46 -0700

    complete game logic

--- src/index.js
+++ src/index.js
@@ -11,25 +11,56 @@ class Board extends React.Component {
     };
   }
 
-  renderSquare(i) {
-    return <Square
-      value={this.state.squares[i]}
-      onClick={() => this.handleClick(i)}
-    />
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let index = 0; index < lines.length; index++) {
+      const [a,b,c] = lines[index];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
+
+
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (this.calculateWinner(squares) || squares[i]) {
+      alert('invalid move');
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
       xIsNext: !this.state.xIsNext,
     })
   }
-
+  renderSquare(i) {
+    return <Square
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+    />
+  }
   render() {
-    let player = this.state.xIsNext ? 'X' : 'O';
-    const status = `Next player: ${player}`;
+    const winner = this.calculateWinner(this.state.squares);
+    console.log(winner);
+    let status;
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      const player = this.state.xIsNext ? 'X' : 'O';
+      status = `Next player: ${player}`;
+    }
     return (
       <div>
         <div className='status'>{status}</div>
@@ -77,3 +108,4 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 )
+
